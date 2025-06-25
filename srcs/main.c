29b19a10084b68738b7ba08a8d94459224a3bd4c
rento yabuki <rento.nky.1203@gmail.com>
2025-06-25 13:34:14 +0900

    挿入フェーズと検索フェーズのロジックを改善し、空行の処理を追加しました

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 13:21:04 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/06/25 13:21:12 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/06/25 13:33:19 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ static char *read_line(char *buf, int *index, int bufsize) {
 		return NULL;
 	while (*index < bufsize && buf[*index] != '\n')
 		(*index)++;
-	if (*index == start)
+	// 空行の場合も処理する（start == *index でもNULLを返さない）
+	if (*index >= bufsize)
 		return NULL;
 	buf[(*index)++] = 0;
 	return buf + start;
@@ -33,27 +34,33 @@ int	main(void)
 	if (n <= 0)
 		fatal_error();
 	int		i = 0;
+	int		insert_phase = 1;
 
 	// Insert phase
 	while (i < n) {
 		char *key = read_line(buffer, &i, n);
-		if (!key || *key == 0) break;
-		char *value = read_line(buffer, &i, n);
-		if (!value) break;
-		insert(key, value);
-	}
-
-	// Search phase
-	while (i < n) {
-		char *query = read_line(buffer, &i, n);
-		if (!query || *query == 0) continue;
-		char *result = search(query);
-		if (result) {
-			write(1, result, str_len(result));
-			write(1, "\n", 1);
+		if (!key) break;
+		
+		// 空行で挿入フェーズ終了
+		if (str_len(key) == 0) {
+			insert_phase = 0;
+			continue;
+		}
+		
+		if (insert_phase) {
+			char *value = read_line(buffer, &i, n);
+			if (!value) break;
+			insert(key, value);
 		} else {
-			write(1, query, str_len(query));
-			write(1, " Not found.\n", 12);
+			// Search phase
+			char *result = search(key);
+			if (result) {
+				write(1, result, str_len(result));
+				write(1, "\n", 1);
+			} else {
+				write(1, key, str_len(key));
+				write(1, " Not found.\n", 12);
+			}
 		}
 	}
 
